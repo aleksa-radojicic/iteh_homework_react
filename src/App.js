a3ac699df969c6595651c54f13730fd0d80e0a21
aleksa-radojicic <aleksa.radojicic@outlook.com>
2023-01-06 15:06:30 +0100

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./components/index-page/Index";
+import NavBar from "./components/layouts/NavBar";
+import Footer from "./components/layouts/Footer";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "EHEIM External Filter",
+      description: "filter for freshwater and marine aquariums",
+      image: "featured1.jpg",
+      image2: "featured1.jpg",
+      image3: "featured1.jpg",
+      image4: "featured1.jpg",
+      price: 149.990000000000009094947017729282379150390625,
+      created_at: "2022-12-30T10:27:02.000000Z",
+      updated_at: "2022-12-30T10:27:02.000000Z",
+    },
+    {
+      id: 2,
+      name: "EHEIM Smart climate controller",
+      description: "the device which controlls climate parameters",
+      image: "featured2.jpg",
+      image2: "featured1.jpg",
+      image3: "featured1.jpg",
+      image4: "featured1.jpg",
+      price: 299.990000000000009094947017729282379150390625,
+      created_at: "2022-12-30T10:27:02.000000Z",
+      updated_at: "2022-12-30T10:27:02.000000Z",
+    },
+    {
+      id: 3,
+      name: "EHEIM Autofeeder",
+      description: "autofeeder from EHEIM",
+      image: "featured3.jpg",
+      image2: "featured1.jpg",
+      image3: "featured1.jpg",
+      image4: "featured1.jpg",
+      price: 39.99000000000000198951966012828052043914794921875,
+      created_at: "2022-12-30T10:27:02.000000Z",
+      updated_at: "2022-12-30T10:27:02.000000Z",
+    },
+    {
+      id: 4,
+      name: "Sera Marine Granules Nature",
+      description: "marine granules nature from Sera",
+      image: "featured4.jpg",
+      image2: "featured2.jpg",
+      image3: "featured3.jpg",
+      image4: "featured4.jpg",
+      price: 5.9900000000000002131628207280300557613372802734375,
+      created_at: "2022-12-30T10:27:02.000000Z",
+      updated_at: "2022-12-30T10:27:02.000000Z",
+    },
+    {
+      id: 5,
+      name: "EHEIM Internal Filter",
+      description: "filter for freshwater and marine aquariums",
+      image: "featured1.jpg",
+      image2: "featured2.jpg",
+      image3: "featured3.jpg",
+      image4: "featured4.jpg",
+      price: 149.990000000000009094947017729282379150390625,
+      created_at: "2022-12-30T10:27:02.000000Z",
+      updated_at: "2022-12-30T10:27:02.000000Z",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
